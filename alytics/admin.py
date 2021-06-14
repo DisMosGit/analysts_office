618fa7f775dc2ae5ce817ae7def55cf14434f8b0
Denis <dissurgu@yandex.ru>
2021-06-14 13:46:53 +0500

    add graph model

--- alytics/admin.py
+++ alytics/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Graph
 
-# Register your models here.
+
+@admin.register(Graph)
+class GraphAdmin(admin.ModelAdmin):
+    pass
--- alytics/models.py
+++ alytics/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Graph(models.Model):
+    func = models.CharField(verbose_name=_("functions"), max_length=255)
+    points = models.JSONField(verbose_name=_("points"), null=True)
+    interval = models.IntegerField(verbose_name=_("interval"), default=7)
+    step = models.IntegerField(verbose_name=_("step"), default=2)
+    date_created = models.DateTimeField(verbose_name=_("crated"),
+                                        auto_now_add=True)
+    date_processed = models.DateTimeField(verbose_name=_("processed"),
+                                          null=True)
